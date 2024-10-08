cec9916fd7773b273c9a1c00280b883cf3b03b09
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-08 18:45:31 -0300

    feat: :sparkles: Criando componente HomeIcon

---
+++ src/components/HomeIcon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeIcon = () => {
+  return(
+    <div  id="home-icon-container">
+      <img id="home-icon-image" alt="Icone" />
+      <p id="home-icon">Celebrate</p>
+    </div>
+  );
+};
+
+export default HomeIcon;
