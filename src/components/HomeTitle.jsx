682bc55c78851a88aba39c5bd0c86cca3dc54a9a
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-08 18:49:10 -0300

    feat: :sparkles: Criando componente HomeTItle

---
+++ src/components/HomeTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeTitle = () => {
+  return (
+    <div id="home-main-title-container">
+      <h1 id="home-main-title">Celebrate</h1>
+      <h2 id="home-main-subtitle">Criando momentos inesquecíveis e unicos</h2>
+    </div>
+  )
+};
+
+export default HomeTitle;
