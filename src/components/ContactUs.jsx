349a3963bd5cb648a416ad3652b91444760aed7f
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-08 18:58:28 -0300

    feat: :sparkles: Criand componente ContactUs

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const  ContactUs = () => {
+  return (
+    <div id="contact-us-container">
+      <input type="button" value="Entre em Contato"  id="contact-us-button"/>
+    </div>
+  );
+};
+
+export default ContactUs
