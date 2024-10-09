aa391b523f236f4f535e8a0fe1abda9a4a1c1b01
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 12:32:36 -0300

    feat: :sparkles: Passando componentes da pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from '../res/logo.svg';
 import '../css/pages/Home.css';
+import HomeIcon from '../components/HomeIcon';
+import HomeTitle from '../components/HomeTitle';
+import ContactUs from '../components/ContactUs';
+import NavBar from '../components/NavBar';
+import Categories from '../components/Categories';
+import About from '../components/About';
 
 function Home() {
   return (
     <div className="Home">
       <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HomeIcon />
+        <HomeTitle />
+        <ContactUs />
+        <NavBar />
+        <Categories />
+        <About />
       </header>
     </div>
   );
