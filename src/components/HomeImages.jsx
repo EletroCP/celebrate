42c08a2dad9e786a1a5d04f98dad176ea9c48370
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 14:47:10 -0300

    feat: :sparkles: Adicionando imagem

--- src/components/HomeImages.jsx
+++ src/components/HomeImages.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import homeImages from "../res/homeImages.svg" 
 const HomeImages = () => {
+
   return (
     <div  id="home-image-container">
-      <img src="" alt="Icones comemorativos" id="home-image+"/>
+      <img src={homeImages} alt="Icones comemorativos" id="home-image+"/>
     </div>
   );
 };
