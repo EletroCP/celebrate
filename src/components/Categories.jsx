0f6379dfbdee7096fa9c9589b0830de34f85a2d4
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 10:30:55 -0300

    feat: :sparkles: Criando componente de categorias

---
+++ src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Categories= () => {
+  return (
+    <div>
+      <ul>
+        <li>Camping e atividades ao ar livre</li>
+        <li>Cestas, tabuas de frios, festa na caixa e presentes</li>
+        <li>Encontros romanticos</li>
+        <li>Festas e eventos</li>
+        <li>Infantil</li>
+        <li>Diversão entre amigos</li>
+        <li>Para toda a familia</li>
+        <li>Geek</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories
