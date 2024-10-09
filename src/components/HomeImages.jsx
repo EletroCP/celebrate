d1e77c2df5dc111238b99f6e28f99fc21a12dc3c
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 09:01:26 -0300

    feat: :sparkles: Criando componente de decoração

---
+++ src/components/HomeImages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeImages = () => {
+  return (
+    <div  id="home-image-container">
+      <img src="" alt="Icones comemorativos" id="home-image+"/>
+    </div>
+  );
+};
+
+export default HomeImages;
