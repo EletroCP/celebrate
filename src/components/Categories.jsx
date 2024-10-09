824ee711b280e7820a66df62f3e5c5bb194282e5
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 14:46:00 -0300

    feat: :lipstick: Adicionando ids e classnames ao componente categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../scripts/ContextProvider";
 
 const Categories= () => {
+  const { hideBurger } = useContext(Context);
   return (
-    <div>
-      <ul>
-        <li>Camping e atividades ao ar livre</li>
-        <li>Cestas, tabuas de frios, festa na caixa e presentes</li>
-        <li>Encontros romanticos</li>
-        <li>Festas e eventos</li>
-        <li>Infantil</li>
-        <li>Diversão entre amigos</li>
-        <li>Para toda a familia</li>
-        <li>Geek</li>
+    <div style={{ display: hideBurger ? 'none' : 'flex'}} id="burger-container">
+      <ul id="burger-ul">
+        <li className="burger-li">Camping e atividades ao ar livre</li>
+        <li className="burger-li">Cestas, tabuas de frios, festa na caixa e presentes</li>
+        <li className="burger-li">Encontros romanticos</li>
+        <li className="burger-li">Festas e eventos</li>
+        <li className="burger-li">Infantil</li>
+        <li className="burger-li">Diversão entre amigos</li>
+        <li className="burger-li">Para toda a familia</li>
+        <li className="burger-li">Geek</li>
       </ul>
     </div>
   );
