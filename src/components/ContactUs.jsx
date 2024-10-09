c6b1319c3951c364dc000fe5545b4c0d4a11322f
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-09 14:46:31 -0300

    feat: :sparkles: Linke de coneção ao contato

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const  ContactUs = () => {
+  const handleClick = () => {
+    window.open("https://wa.me/message/CYT6YKIGPYCNC1", "_blank");
+  };
   return (
     <div id="contact-us-container">
-      <input type="button" value="Entre em Contato"  id="contact-us-button"/>
+      <input type="button" value="Entre em Contato" id="contact-us-button" onClick={handleClick}/>
     </div>
   );
 };
