fb93018325c1af2ddcccd62ae5faa1b02c9af640
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-10 14:36:34 -0300

    fix: :zap: Corrigindo utilização de link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,8 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { Context } from "../scripts/ContextProvider";
 
 const NavBar = () => {
   const { hideBurger, setHideBurger } = useContext(Context);
-
-  const navigate = useNavigate();
 
   const burgerHandler = () => {
     setHideBurger(!hideBurger);
@@ -15,9 +12,9 @@ const NavBar = () => {
     <div>
       <nav>
         <a id="nav-button-about" className="nav-buttons" href="#about-container">Sobre</a>
-        <a id="nav-button-signature" className="nav-buttons" onClick={() => navigate('/signature')}>Assinatura</a>
+        <a id="nav-button-signature" className="nav-buttons" href='/signature'>Assinatura</a>
         <a id="nav-button-kits" className="nav-buttons" href="#kits-container">Kit Prontos</a>
-        <a id="nav-button-customize" className="nav-buttons" onClick={() => burgerHandler()}>Personalize</a>
+        <p id="nav-button-customize" className="nav-buttons" onClick={() => burgerHandler()}>Personalize</p>
       </nav>
     </div>
   );
